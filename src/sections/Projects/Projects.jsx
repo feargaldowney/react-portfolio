bded968c4e0dff18d5d3c31bcc68a72975f227b2
Feargal <Feargaldowney123@gmail.com>
2024-11-07 13:17:48 +0000

    finishing touches

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -2,9 +2,8 @@ import styles from './ProjectsStyles.module.css'
 import gameRoom from '../../assets/game-room.png';
 import viberr from '../../assets/viberr.png';
 import freshBrgr from '../../assets/fresh-burger.png';
-import spring from '../../assets/spring.png';
+import house from '../../assets/house.png';
 import ProjectCard from '../../common/ProjectCard';
-
 
 
 function Projects() {
@@ -16,25 +15,25 @@ function Projects() {
             src={gameRoom}
             link="https://github.com/feargaldowney/finalyearproject"
             h3="Helping Tim to Talk"
-            p="Gamified Speech Therapy App"
-            />
-            <ProjectCard 
-            src={viberr} 
-            link="https://github.com/feargaldowney/Jammming"
-            h3="Jammming"
-            p="Spotify Playlist Creator Web App"
-            />
-            <ProjectCard 
-            src={spring} 
-            link="https://github.com/feargaldowney/spring"
-            h3="Rental Management Service"
-            p="A RESTful web service using Spring Boot and JPA"
+            p="Gamified Speech Therapy App."
             />
             <ProjectCard 
             src={freshBrgr} 
             link="https://github.com/feargaldowney/smithereens"
             h3="Smithereens"
-            p="Web app for band featuring ticket sales"
+            p="Web app for a rock band."
+            />
+            <ProjectCard 
+            src={viberr} 
+            link="https://github.com/feargaldowney/Jammming"
+            h3="Jammming"
+            p="Spotify Playlist Creator Web App."
+            />
+            <ProjectCard 
+            src={house} 
+            link="https://github.com/feargaldowney/spring"
+            h3="Rental Management Service"
+            p="A RESTful web service using Spring Boot and JPA."
             />
       
         </div>
